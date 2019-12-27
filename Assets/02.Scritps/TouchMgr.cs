faeb00a3b4793aecae06a71abdf291421ffbd95b
Jaehyun Lee <myleje@gmail.com>
2019-12-27 15:00:52 +0900

    .

--- Assets/02.Scritps/TouchMgr.cs
+++ Assets/02.Scritps/TouchMgr.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using GoogleARCore;
 
 public class TouchMgr : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Camera arCamera;
+    public GameObject monster;
+
     void Start()
     {
         
@@ -13,6 +16,22 @@ public class TouchMgr : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.touchCount == 0) return;
+
+        Touch touch = Input.GetTouch(0);
+
+        TrackableHit hit;
+        TrackableHitFlags mask = TrackableHitFlags.FeaturePointWithSurfaceNormal
+                                | TrackableHitFlags.PlaneWithinPolygon;
+
+        if (touch.phase == TouchPhase.Began
+            && Frame.Raycast(touch.position.x, touch.position.y, mask, out hit))
+        {
+            var anchor = hit.Trackable.CreateAnchor(hit.Pose);
+            GameObject _monster = Instantiate(monster
+                                            , hit.Pose.position
+                                            , Quaternion.identity
+                                            , anchor.transform);
+        } 
     }
 }
